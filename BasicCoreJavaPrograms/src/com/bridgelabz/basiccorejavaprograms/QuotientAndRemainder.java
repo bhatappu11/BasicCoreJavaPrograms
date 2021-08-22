ec190c99fb40fcd3818eed84c0077fcc793ce6f1
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-22 15:25:29 +0530

    ADDED: code to find quotient and remainder

---
+++ BasicCoreJavaPrograms/src/com/bridgelabz/basiccorejavaprograms/QuotientAndRemainder.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.basiccorejavaprograms;
+
+public class QuotientAndRemainder {
+
+	public static void main(String[] args) {
+		int dividend = 652, divisor = 5;
+		  
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+  
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+
+	}
+
+}
